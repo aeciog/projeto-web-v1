5ecb524b3cb40f22f768b7f476fa9018b08fc7d4
Aécio Guterres <aecioguterres@gmail.com>
2023-02-09 10:51:32 -0300

    Subindo aplicação

---
+++ src/main/java/com/aetiosdigital/projetowebv1/ProjetoWebV1Application.java
@@ -0,0 +1,13 @@
+package com.aetiosdigital.projetowebv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/aetiosdigital/projetowebv1/ProjetoWebV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aetiosdigital.projetowebv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
