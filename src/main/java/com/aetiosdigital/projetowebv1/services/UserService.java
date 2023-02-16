f3461913184e9e3f2b7fbb3120d5aad6c93a1b04
Aécio Guterres <aecioguterres@gmail.com>
2023-02-16 19:03:06 -0300

    Tratamento de exception - findById

--- src/main/java/com/aetiosdigital/projetowebv1/services/UserService.java
+++ src/main/java/com/aetiosdigital/projetowebv1/services/UserService.java
@@ -3,6 +3,7 @@ package com.aetiosdigital.projetowebv1.services;
 import com.aetiosdigital.projetowebv1.entities.User;
 import com.aetiosdigital.projetowebv1.repositories.UserRepository;
 
+import com.aetiosdigital.projetowebv1.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
